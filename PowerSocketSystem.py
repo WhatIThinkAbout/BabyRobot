cf053c98a069a68615a6f1ae11951173f1b61823
WhatIThinkAbout <steve@steveroberts.name>
2020-07-13 17:22:25 +0100

    Modified base classes

--- PowerSocketSystem.py
+++ PowerSocketSystem.py
@@ -1,9 +1,12 @@
 import random
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import scipy.stats as stats
 import seaborn as sns
+import matplotlib
+import matplotlib.pyplot as plt
 from tqdm import tqdm
+from IPython.core.pylabtools import figsize
 
 """
     System Setup
@@ -17,6 +20,15 @@ socket_means = [((q*2)+2) for q in socket_order]
 
 # save the number of sockets
 NUM_SOCKETS = len(socket_order)
+
+
+"""
+    Display Setup
+"""
+
+# display all floating point numbers to 3 decimal places
+np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+
 
 """
     Helper Functions
@@ -36,112 +48,203 @@ class PowerSocket:
     """ the base power socket class """
     
     def __init__(self, q):                
-        self.q = q   # the true reward value                
+        self.q = q        # the true reward value              
+        self.initialize() # reset the socket
+        
+    def initialize(self):
         self.Q = 0   # the estimate of this socket's reward value                
-        self.n = 0   # the number of times this socket has been tried
+        self.n = 0   # the number of times this socket has been tried        
     
-    # the reward is a random distribution around the initial mean value set for this socket
-    # - never allow a charge less than 0 to be returned
     def charge(self):
-        # a guassian distribution with unit variance around the true value 'q'
+        """ return a random amount of charge """
+        
+        # the reward is a guassian distribution with unit variance around the true
+        # value 'q'
         value = np.random.randn() + self.q        
+        
+        # never allow a charge less than 0 to be returned        
         return 0 if value < 0 else value
                     
-    # increase the number of times this socket has been used and improve the estimate of the 
-    # value (the mean) by combining the new reward 'r' with the current mean
     def update(self,R):
+        """ update this socket after it has returned reward value 'R' """     
+    
+        # increment the number of times this socket has been tried
         self.n += 1
 
-        # the new estimate is calculated from the old estimate
+        # the new estimate of the mean is calculated from the old estimate
         self.Q = (1 - 1.0/self.n) * self.Q + (1.0/self.n) * R
-
-    # return the estimate of the socket's reward value
+    
     def sample(self):
+        """ return an estimate of the socket's reward value """
         return self.Q
-
-
-# Create an Optimistic Power Socket class by inheriting from the standard Power Socket
-class OptimisticPowerSocket( PowerSocket ):
-    def __init__( self, q, initial_estimate = 0. ):    
-        
-        # pass the true reward value to the base PowerSocket             
-        super().__init__(q)        
-        
-        # estimate of this socket's reward value 
-        # - set to supplied initial value
-        self.Q = initial_estimate                 
-        
-        # the number of times this socket has been tried 
-        # - set to 1 if an initialisation value is supplied
-        self.n = 1 if initial_estimate > 0 else 0                
+            
 
 
 class SocketTester():
+    """ create and test a set of sockets over a single test run """
 
-    def __init__(self, socket=OptimisticPowerSocket, socket_order=socket_order, initial_estimate = 0. ):  
+    def __init__(self, socket=PowerSocket, socket_order=socket_order, **kwargs ):  
         
-        # create the supplied socket type with a mean value defined by the socket order       
-        self.sockets = [socket((q*2)+2, initial_estimate) for q in socket_order]                  
+        # create supplied socket type with a mean value defined by socket order 
+        self.sockets = [socket((q*2)+2, **kwargs) for q in socket_order]     
         
         # set the number of sockets equal to the number created
         self.number_of_sockets = len(self.sockets)           
 
         # the index of the best socket is the last in the socket_order list
         # - this is a one-based value so convert to zero-based
-        self.optimal_socket_index = (socket_order[-1] - 1)     
+        self.optimal_socket_index = (socket_order[-1] - 1)   
         
-        # store any initial estimate used to initialize the sockets
-        self.initial_estimate = initial_estimate
-                     
-       
-    def run( self, number_of_steps ):            
+        # by default a socket tester records 2 bits of information over a run
+        self.number_of_stats = kwargs.pop('number_of_stats', 2)       
+             
+            
+    def initialize_run(self, number_of_steps):
+        """ reset counters at the start of a run """
         
-        # save data about the run to create a table of estimates
-        estimates = np.zeros(shape=(number_of_steps+1, self.number_of_sockets))
-
-        # set the first estimates to be the initial value
-        estimates[0] = self.initial_estimate 
-
+        # save the number of steps over which the run will take place
+        self.number_of_steps = number_of_steps
+        
         # monitor the total reward obtained over the run
-        total_reward = 0
+        self.total_reward = 0
+           
+        # stats for each time-step
+        # - by default records: estimate, number of trials
+        self.socket_stats = np.zeros(shape=(number_of_steps+1, 
+                                            self.number_of_sockets, 
+                                            self.number_of_stats))
+        
+        # ensure that all sockets are re-initialized
+        for socket in self.sockets: socket.initialize()
+            
+                                
+    def charge_and_update(self,socket_index):
+        """ charge from & update the specified socket and associated parameters """
+        
+        # charge from the chosen socket and update its mean reward value
+        reward = self.sockets[socket_index].charge()
+        self.sockets[socket_index].update(reward)
 
-        # monitor the number of times the optimal socket was chosen
-        optimal_socket_selected = 0
+        # update the total reward
+        self.total_reward += reward        
+        
+        
+    def get_socket_stats( self, t ):
+        """ get the current information from each socket """        
+        socket_stats = [[socket.Q, socket.n] for socket in self.sockets]
+        return socket_stats     
+    
+    def get_mean_reward( self ):
+        """ the total reward averaged over the number of time steps """
+        return (self.total_reward/self.number_of_steps)
+    
+    def get_estimates(self):
+        return self.socket_stats[:,:,0][-1]
+        
+    def get_socket_percentages( self ):
+        """ get the percentage of times each socket was tried over the run """
+        return (self.socket_stats[:,:,1][-1]/self.number_of_steps)
+    
+    def get_optimal_socket_percentage( self ):
+        """ get the percentage of times the optimal socket was tried """        
+        final_trials = self.socket_stats[:,:,1][-1]
+        return (final_trials[self.optimal_socket_index]/self.number_of_steps)
+    
 
-        # keep a count of the number of times each socket was selected
-        selected_socket = np.zeros(shape=(self.number_of_sockets))   
+    def select_socket( self, t ):
+        """ Greedy Socket Selection"""
+        
+        # choose the socket with the current highest mean reward or arbitrarily
+        # select a socket in the case of a tie            
+        socket_index = random_argmax([socket.sample() for socket in self.sockets]) 
+        return socket_index     
+    
+    
+    def run( self, number_of_steps ):  
+        """ perform a single run, over the set of sockets, 
+            for the defined number of steps """
+        
+        # reset the run counters
+        self.initialize_run(number_of_steps)
         
         # loop for the specified number of time-steps
         for t in range(number_of_steps):
 
+            # get information about all sockets at the start of the time step
+            self.socket_stats[t] = self.get_socket_stats(t)            
+            
             # select a socket
             socket_index = self.select_socket(t)
-
+            
             # charge from the chosen socket and update its mean reward value
-            reward = self.sockets[socket_index].charge()
-            self.sockets[socket_index].update(reward)
-
-            # store the estimates of each socket at this time step
-            for socket_number in range(self.number_of_sockets):
-                estimates[t+1,socket_number] = self.sockets[socket_number].Q 
-
-            # update the total reward
-            total_reward += reward
-
-            # monitor the number of times the optimal socket was chosen
-            if socket_index == self.optimal_socket_index: 
-                optimal_socket_selected += 1      
-
-            # increment the count of the number of times the selected socket has been used
-            selected_socket[socket_index] += 1
-
-        return estimates, (total_reward/number_of_steps), (optimal_socket_selected/number_of_steps), (selected_socket/number_of_steps)    
+            self.charge_and_update(socket_index)
+       
+        # get the stats for each socket at the end of the run
+        self.socket_stats[number_of_steps] = self.get_socket_stats(number_of_steps)        
+       
     
     
-    def select_socket( self, t ):
-        """ Greedy Socket Selection"""
+class SocketExperiment():
+    """ setup and run repeated socket tests to get the average results """
+    
+    def __init__(self, 
+                 socket_tester = SocketTester,
+                 number_of_tests = 1000,
+                 number_of_steps = 30,                      
+                 **kwargs ):  
         
-        # choose the socket with the current highest mean reward or arbitrary select a socket in the case of a tie            
-        socket_index = random_argmax([socket.sample() for socket in self.sockets])  
+        self.socket_tester = socket_tester
+        self.number_of_tests = number_of_tests
+        self.number_of_steps = number_of_steps    
+        self.number_of_sockets = self.socket_tester.number_of_sockets
         
-        return socket_index        
+    def initialize_run(self):
+        
+        # keep track of the average values over the run
+        self.reward_per_timestep = 0.
+        self.optimal_selected = 0.                
+        self.socket_percentages = np.zeros(self.number_of_sockets)   
+        self.estimates = np.zeros(shape=(self.number_of_steps+1,self.number_of_sockets))
+        
+    def get_reward_per_timestep(self):
+        """ the mean reward per timestep """
+        return self.reward_per_timestep
+
+    def get_optimal_selected(self):
+        """ the mean times the optimal socket was selected """
+        return self.optimal_selected
+
+    def get_socket_percentages(self): 
+        """ the mean of the percentage times each socket was selected """
+        return self.socket_percentages
+    
+    def get_estimates(self):
+        """ per socket reward estimates and number of trials """
+        return self.estimates
+    
+    def update_mean( self, current_mean, new_value, n ):
+        """ calculate the new mean from the previous mean and the new value """
+        return (1 - 1.0/n) * current_mean + (1.0/n) * new_value
+        
+    def record_test_stats(self,n):
+        """ update the mean value for each statistic being tracked over a run """
+
+        # calculate the new means from the old means and the new value
+        tester = self.socket_tester
+        self.reward_per_timestep = self.update_mean( self.reward_per_timestep, tester.get_mean_reward(), n)
+        self.optimal_selected = self.update_mean( self.optimal_selected, tester.get_optimal_socket_percentage(), n)
+        self.socket_percentages = self.update_mean( self.socket_percentages, tester.get_socket_percentages(), n)
+        self.estimates = self.update_mean( self.estimates, tester.get_estimates(), n)
+        
+    
+    def run(self):
+        """ repeat the test over a set of sockets for the specified number of trials """
+                    
+        # do the specified number of runs for a single test
+        self.initialize_run()
+        for n in range(1,self.number_of_tests+1):
+
+            # do one run of the test                               
+            self.socket_tester.run( number_of_steps = self.number_of_steps )     
+            self.record_test_stats(n)                         
+           
